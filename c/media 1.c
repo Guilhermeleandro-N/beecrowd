450886972ff76d8107169e783866b45a17765270
Guilhermeleandro-N <135875294+Guilhermeleandro-N@users.noreply.github.com>
2024-01-26 07:43:12 -0300

    Create media 1.c

---
+++ c/media 1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double a,b,media;
+    scanf("%lf.1",&a);
+    scanf("%lf.1",&b);
+    media = (a*3.5+b*7.5)/11;
+    printf("MEDIA = %.5lf\n",media);
+    return 0;
+    
+}
